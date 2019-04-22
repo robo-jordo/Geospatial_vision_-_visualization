ea362bb9c08f08351162689e140609cd23ae69d9
Levi Todes <levitodes2019@u.northwestern.edu>
2019-04-22 16:56:01 -0500

    highlighting the smudge

--- HW1/smudges.py
+++ HW1/smudges.py
@@ -12,8 +12,10 @@ path=Path(".")
 path=path.glob("hey/*.jpg")
 
 images=[]
+finalarray=[]
 backSub1 = cv2.createBackgroundSubtractorMOG2()
 backSub2 = cv2.createBackgroundSubtractorKNN()
+backSub3 = cv2.bgsegm.createBackgroundSubtractorMOG()
 # path=Path(".")
 # files=(path.glob(("hey/*.jpg")).sort()
 # print(files)
@@ -67,6 +69,13 @@ for i in images:
 	final2 = np.concatenate((thresh3, thresh4), axis=1)
 	final = np.concatenate((final1, final2), axis=0)
 	final = cv2.medianBlur(final,5)
+	finalarray.append(final)
+	# ret,newthresh = cv2.threshold(final,200,255,cv2.THRESH_BINARY)
+
+	# new =cv2.adaptiveThreshold(final,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 101, 11)
+	
+	
+
 	# clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
 	# cl1 = clahe.apply(i)
 	# cl1*1.2
@@ -80,15 +89,49 @@ for i in images:
 	# fgMask2 = backSub2.apply(median)
 	# #fgMask3 = backSub1.apply(fgMask2)
 	# #fgMask4 = backSub1.apply(fgMask3)
-	cv2.imshow('Frame', i)
-	cv2.imshow('cl1', img_output)
-	
-	#cv2.imshow('median', median)
-	cv2.imshow('q1', thresh1)
-	cv2.imshow('q2', thresh2)
-	cv2.imshow('q3', thresh3)
-	cv2.imshow('q4', thresh4)
-	cv2.imshow('fin', final)
+
+	# cv2.imshow('Frame', i)
+	# cv2.imshow('cl1', img_output)
+	# cv2.imshow('q1', thresh1)
+	# cv2.imshow('q2', thresh2)
+	# cv2.imshow('q3', thresh3)
+	# cv2.imshow('q4', thresh4)
+	# cv2.imshow('fin', final)
+
 	# cv2.imshow('final2', fgMask2)
 	#cv2.imshow('FG Mask4', fgMask4)
+	# cv2.waitKey(0)
+
+smear_min_r =  6
+smear_max_r =  10
+smear_min_a =  3.14*smear_min_r**2
+smear_max_a =  3.14*smear_max_r**2
+avg = np.zeros((400,400),np.float)
+count = 0
+
+for i in range(len(images)):
+	avg += np.array(finalarray[i],dtype=np.float)
+	count +=1
+	if count==20 or i ==0:
+		newavg = avg/count
+		newavg = np.array(np.round(newavg),dtype=np.uint8)
+		threshnew = cv2.adaptiveThreshold(newavg,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 101, 101)
+		# ret,threshnew = cv2.threshold(final,200,255,cv2.THRESH_BINARY)
+		newinv = cv2.bitwise_not(threshnew)
+		avg = np.zeros((400,400),np.float)
+		count = 0
+
+	#Contours
+	_,contours,_ = cv2.findContours(newinv,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) 
+
+	if contours:
+		if (cv2.contourArea(contours[0])>smear_min_a and cv2.contourArea(contours[0])<smear_max_a):
+			smudge = cv2.drawContours(images[i],contours,-1,(0,255,0),2) 
+			cv2.imshow('contour', smudge)
+			cv2.waitKey(0)
+
+	cv2.imshow('zefsothresh',newavg)
+	cv2.imshow('invvv', newinv)
+	cv2.imshow('final',images[i])
+	# cv2.imshow('cover',coverblur)
 	cv2.waitKey(0)
