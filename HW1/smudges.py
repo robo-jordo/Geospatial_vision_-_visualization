bf6cdb8c61dce3d49253f6b560dd58b52eaa3321
Jordan Haskel <jordhask@gmail.com>
2019-04-22 12:47:47 -0500

    Geospatial homework 1 preprocessing for smudge masking

---
+++ HW1/smudges.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+
+image_split = 4
+import cv2
+from pathlib import Path
+import glob
+import numpy as np
+
+path=Path(".")
+
+path=path.glob("hey/*.jpg")
+
+images=[]
+backSub1 = cv2.createBackgroundSubtractorMOG2()
+backSub2 = cv2.createBackgroundSubtractorKNN()
+# path=Path(".")
+# files=(path.glob(("hey/*.jpg")).sort()
+# print(files)
+
+filenames = glob.glob("hey/*.jpg")
+filenames.sort()
+
+#images = [cv2.imread(img) for img in filenames]
+# for img in images:
+#     print img
+
+for imagepath in filenames:
+		img=cv2.imread(str(imagepath))
+		print(imagepath)
+		#img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)                         
+		img=cv2.resize(img,(400,400))
+		images.append(img)
+
+for i in images:
+	img_yuv = cv2.cvtColor(i, cv2.COLOR_BGR2YUV)
+
+# equalize the histogram of the Y channel
+	img_yuv[:,:,0] = cv2.equalizeHist(img_yuv[:,:,0])
+
+# convert the YUV image back to RGB format
+	img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
+	img_output = cv2.cvtColor(img_output,cv2.COLOR_BGR2GRAY)
+
+	median = cv2.medianBlur(img_output,3)
+	height, width, channels = img.shape
+	median1 = img_output[:width/2,:height/2]
+	median2 = img_output[:width/2,height/2:]
+	median3 = img_output[width/2:,:height/2]
+	median4 = img_output[width/2:,height/2:]
+	
+	mean1 = (median[:width/2,:height/2]).mean()
+	mean2 = (median[:width/2,height/2:]).mean()
+	mean3 = (median[width/2:,:height/2]).mean()
+	mean4 = (median[width/2:,height/2:]).mean()
+	print(mean1)
+
+	ret1,thresh1 = cv2.threshold(median1,mean1-(mean1/4),255,cv2.THRESH_BINARY)
+	ret2,thresh2 = cv2.threshold(median2,mean2-(mean2/4),255,cv2.THRESH_BINARY)
+	ret3,thresh3 = cv2.threshold(median3,mean3-(mean3/4),255,cv2.THRESH_BINARY)
+	ret4,thresh4 = cv2.threshold(median4,mean4-(mean4/4),255,cv2.THRESH_BINARY)
+	# median1 = cv2.medianBlur(thresh1,5)
+	# median2 = cv2.medianBlur(thresh2,5)
+	# median3 = cv2.medianBlur(thresh3,5)
+	# median4 = cv2.medianBlur(thresh4,5)
+	final1 = np.concatenate((thresh1, thresh2), axis=1)
+	final2 = np.concatenate((thresh3, thresh4), axis=1)
+	final = np.concatenate((final1, final2), axis=0)
+	final = cv2.medianBlur(final,5)
+	# clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+	# cl1 = clahe.apply(i)
+	# cl1*1.2
+	# median = cv2.medianBlur(cl1,3)
+	# fgMask = backSub1.apply(median)
+	# edges1 = cv2.Canny(median,100,200)
+
+	# fgMask = backSub1.apply(equ)
+	# edges2 = cv2.Canny(fgMask,0,200)
+	# #median = cv2.medianBlur(edges2,5)
+	# fgMask2 = backSub2.apply(median)
+	# #fgMask3 = backSub1.apply(fgMask2)
+	# #fgMask4 = backSub1.apply(fgMask3)
+	cv2.imshow('Frame', i)
+	cv2.imshow('cl1', img_output)
+	
+	#cv2.imshow('median', median)
+	cv2.imshow('q1', thresh1)
+	cv2.imshow('q2', thresh2)
+	cv2.imshow('q3', thresh3)
+	cv2.imshow('q4', thresh4)
+	cv2.imshow('fin', final)
+	# cv2.imshow('final2', fgMask2)
+	#cv2.imshow('FG Mask4', fgMask4)
+	cv2.waitKey(0)
